7920b7e4bfb9e914a5c4aa182e0d0917caee5778
Alex Key <alex@reservationgenie.com>
2019-06-07 17:44:55 -0500

    cleaned up

--- rails_install.sh
+++ rails_install.sh
@@ -9,9 +9,6 @@ echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/source
 sudo apt-get update
 sudo apt-get install git-core zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev software-properties-common libffi-dev nodejs yarn
 
-echo "Setting up mysql"
-sudo apt-get install mysql-server mysql-client libmysqlclient-dev
-
 echo "Installs RVM (Ruby Version Manager) for handling Ruby installation"
 sudo apt-get install libgdbm-dev libncurses5-dev automake libtool bison libffi-dev
 gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
@@ -19,10 +16,16 @@ curl -sSL https://get.rvm.io | bash -s stable
 source ~/.rvm/scripts/rvm
 rvm install 2.6.3
 rvm use 2.6.3 --default
-ruby -v
 
 
 gem install bundler
 gem install rails -v 6.0.0.rc1
 
+echo "Setting up mysql"
+sudo apt-get install mysql-server mysql-client libmysqlclient-dev
+
+
 echo "Congrats! You are all set with Ruby and MySQL/Sqlite"
+ruby -v
+rails -v
+mysql --version
