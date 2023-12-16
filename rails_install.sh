66d2de6c5c3058eadffed4ec1e2fde02d9bfbc4b
Alex Key <alex@reservationgenie.com>
2023-12-16 12:20:52 -0600

    Update rails_install.sh
    Splitting script into 2 for reload

--- rails_install.sh
+++ rails_install.sh
@@ -16,19 +16,4 @@ sudo apt-get update
 sudo apt-get install rvm
 sudo usermod -a -G rvm $USER
 echo 'source "/etc/profile.d/rvm.sh"' >> ~/.bashrc
-rvm install 3.2.2
-rvm use 3.2.2 --default
-
-
-gem install bundler
-gem install rails -v 7.1.2
-
-echo "Setting up mysql"
-sudo apt-get install postgresql postgresql-contrib libpq-dev
-
-
-echo "Congrats! You are all set with Ruby and MySQL/Sqlite"
-ruby -v
-rails -v
-bundler -v
-postgres --version
+echo 'Please Reboot'
