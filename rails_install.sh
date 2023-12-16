228ea34ee9af5f90cba7409c5c3fa6b53ab69863
Alex Key <alex@reservationgenie.com>
2023-12-16 12:19:12 -0600

    Update rails_install.sh
    Updated to Rails 7.1.2 and Ruby 3.2.2 and Postgres over mysql

--- rails_install.sh
+++ rails_install.sh
@@ -2,7 +2,7 @@ echo "Updates packages. Asks for your password."
 sudo apt-get update -y
 
 echo "Installs packages. Give your password when asked."
-curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
+curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
 
@@ -10,22 +10,25 @@ sudo apt-get update
 sudo apt-get install git-core zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev software-properties-common libffi-dev nodejs yarn
 
 echo "Installs RVM (Ruby Version Manager) for handling Ruby installation"
-sudo apt-get install libgdbm-dev libncurses5-dev automake libtool bison libffi-dev
-gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
-curl -sSL https://get.rvm.io | bash -s stable
-source ~/.rvm/scripts/rvm
-rvm install 2.6.3
-rvm use 2.6.3 --default
+sudo apt-get install software-properties-common
+sudo apt-add-repository -y ppa:rael-gc/rvm
+sudo apt-get update
+sudo apt-get install rvm
+sudo usermod -a -G rvm $USER
+echo 'source "/etc/profile.d/rvm.sh"' >> ~/.bashrc
+rvm install 3.2.2
+rvm use 3.2.2 --default
 
 
 gem install bundler
-gem install rails -v 6.0.0.rc1
+gem install rails -v 7.1.2
 
 echo "Setting up mysql"
-sudo apt-get install mysql-server mysql-client libmysqlclient-dev
+sudo apt-get install postgresql postgresql-contrib libpq-dev
 
 
 echo "Congrats! You are all set with Ruby and MySQL/Sqlite"
 ruby -v
 rails -v
-mysql --version
+bundler -v
+postgres --version
